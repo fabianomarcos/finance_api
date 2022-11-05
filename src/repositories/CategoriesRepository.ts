05a8260e925b3976fbe0614211dc97d1452002b2
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-05 17:54:59 -0300

    feature: created category repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,35 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+	name: string;
+	description: string;
+}
+
+class CategoryRepository {
+	private categories: Category[];
+
+	constructor() {
+		this.categories = [];
+	}
+
+	create({ name, description }: ICreateCategoryDTO): void {
+		const category = new Category();
+
+		Object.assign(category, { name, description, created_at: new Date() });
+
+		this.categories.push(category);
+
+		console.log(category);
+	}
+
+	list(): Category[] {
+		return this.categories;
+	}
+
+	findByName(name: string): Category | undefined {
+		const category = this.categories.find((category) => category.name === name);
+		return category;
+	}
+}
+
+export { CategoryRepository }
