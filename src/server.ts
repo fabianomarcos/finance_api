2c73daf1cc652dfca3c4c4f5801ea5f19ad91204
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-05 20:13:29 -0300

    feature: added specification route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+
 import { categoriesRoutes } from "./routes/categories.route";
+import { specificationRoutes } from "./routes/specification.route";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specification", specificationRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
