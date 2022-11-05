3d262e89d64146e405f72d3d2425dc553c329e20
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-05 20:12:34 -0300

    feature: created specification repositories

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../model/Category";
+import { Specification } from "../model/Specification";
+import { ICreateSpecificationDTO } from "./ISpecificationRepository";
+import { ISpecificationRepository } from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+	private specifications: Specification[] = [];
+
+	constructor() {
+		this.specifications = [];
+	}
+
+	findByName(name: string): Specification | undefined {
+		return this.specifications.find(spec => spec.name === name);
+	}
+
+	list(): Category[] {
+		throw new Error("Method not implemented.");
+	}
+
+	create({ name, description }: ICreateSpecificationDTO): void {
+		const specification =  new Specification();
+
+		Object.assign(specification, { name, description, created_at: new Date() });
+
+		this.specifications.push(specification);
+	}
+}
+
+export { SpecificationRepository }
