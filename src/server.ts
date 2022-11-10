16c5e93316aec247dd0156915285eddb57940ca1
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-10 13:47:44 -0300

    chore: fix configurations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
+import "reflect-metadata";
 import express from "express";
 import swagger from "swagger-ui-express";
 
 import "./database";
-
 import "./shared/container";
-
 import { router } from "./routes";
-
 import swaggerFile from "./swagger.json";
 
 const app = express();
