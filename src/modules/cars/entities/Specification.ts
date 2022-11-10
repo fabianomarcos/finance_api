d42e8801be322b0e16741e66c40eca35270714c9
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-10 11:35:16 -0300

    feature: mapping specification entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,18 @@
-import { v4 as uuid } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
 
+@Entity("specifications")
 class Specification {
+	@PrimaryColumn()
 	id?: string;
+
+	@Column()
 	name: string;
+
+	@Column()
 	description: string;
+
+	@CreateDateColumn()
 	created_at: Date;
 
 	constructor() {
@@ -12,4 +21,3 @@ class Specification {
 }
 
 export { Specification };
-
