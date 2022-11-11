2dc226cd8ddd8b5d884f9c83500ae76e2ffc40ee
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-11 10:40:59 -0300

    feature: instance of errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
-import express from "express";
-import swagger from "swagger-ui-express";
+import express, { Response, Request, NextFunction } from "express";
+import "express-async-errors";
 
+import swagger from "swagger-ui-express";
 import "./database";
 import "./shared/container";
 import { router } from "./routes";
+import { AppError } from "./errors/AppError";
 import swaggerFile from "./swagger.json";
 
 const app = express();
@@ -14,5 +16,14 @@ app.use(express.json());
 app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+	if (err instanceof AppError)
+		return response.status(err.statusCode).json({ message: err.message });
+
+	return response
+		.status(500)
+		.json({ message: `Internal server error - ${err.message}` });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
