3fdee2810be7d04ecdcd8670ed11f1921d09dfda
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-15 19:10:31 -0300

    feature: created migration createCategory

--- src/modules/cars/model/Category.ts
+++ src/modules/cars/model/Category.ts
@@ -1,15 +1,15 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
-class Category {
+export class Category {
 	id?: string;
-	name: string;
-	description: string;
-	created_at: Date;
+
+	name!: string;
+
+	description!: string;
+
+	created_at!: Date;
 
 	constructor() {
 		if (!this.id) this.id = uuid();
 	}
 }
-
-export { Category };
-
--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,13 +1,11 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-class ListCategoriesController {
+export class ListCategoriesController {
 	constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
 
-	handle(request: Request, response: Response): Response {
+	handle(_: Request, response: Response): Response {
 		const all = this.listCategoriesUseCase.execute();
 		return response.json(all);
 	}
 }
-
-export { ListCategoriesController };
