5e2f53fbbeab344ed3db62fd105907046a472fd6
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-11-15 19:30:48 -0300

    refactor: lint

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,12 +12,14 @@ const upload = multer({
 
 categoriesRoutes.post("/", (request, response) => {
 	return createCategoryController.handle(request, response);
-})
+});
 
 categoriesRoutes.get("/", (request, response) => {
 	return listCategoriesController.handle(request, response);
-})
+});
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => importCategoryController.handle(request, response))
+categoriesRoutes.post("/import", upload.single("file"), (request, response) =>
+	importCategoryController.handle(request, response)
+);
 
 export { categoriesRoutes };
